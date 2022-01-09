ba246069cffa6aa0aefdc98b022c732753815c47
ppotatoG <thgus2270@gmail.com>
2022-01-09 23:51:15 +0900

    Add pages

---
+++ front/pages/profile.js
@@ -0,0 +1,7 @@
+const profile = () => {
+    return (
+        <div>내 프로필</div>
+    );
+}
+
+export default profile;
---
+++ front/pages/signup.js
@@ -0,0 +1,7 @@
+const signup = () => {
+    return (
+        <div>회원가입</div>
+    );
+}
+
+export default signup;
