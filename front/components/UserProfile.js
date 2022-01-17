2d6ab33eae5b1143f0c2ebe16a662e0f646541ce
ppotatoG <thgus2270@gmail.com>
2022-01-17 23:46:40 +0900

    Add UserProfile

---
+++ front/components/UserProfile.js
@@ -0,0 +1,12 @@
+import React, {useState, useCallback, useMemo} from 'react';
+import {Form, Input, Button} from 'antd';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const UserProfile = () => {
+    return (
+        <UserProfile />
+    )
+}
+
+export default UserProfile;
