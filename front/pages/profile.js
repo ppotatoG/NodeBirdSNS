35e22aeb7ec3620297d5c4827be835cc0f1aa2c4
ppotatoG <thgus2270@gmail.com>
2022-01-23 17:34:00 +0900

    프로필 페이지 만들기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,15 +1,30 @@
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 import Head from 'next/head';
 
 const profile = () => {
+    const followerList = [
+        {nickname: '제로초'}, 
+        {nickname: '바보'}, 
+        {nickname: '노드버드오피셜'}
+    ];
+
+    const followingList = [
+        {nickname: '제로초'}, 
+        {nickname: '바보'}, 
+        {nickname: '노드버드오피셜'}
+    ];
+
     return (
         <>
             <Head>
-                <meta charSet="utf-8"></meta>
                 <title>내 프로필 | NodeBird</title>
             </Head>
             <AppLayout>
-                <div>내 프로필</div>
+                <NicknameEditForm />
+                <FollowList header="팔로잉 목록" data={followingList}/>
+                <FollowList header="팔로워 목록" data={followerList}/>
             </AppLayout>
         </>
     );
